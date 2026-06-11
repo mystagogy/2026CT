ef1dc32dbe0323bc99bb92844300db2fbc949db3
mystagogy <108065594+mystagogy@users.noreply.github.com>
2026-06-11 14:31:30 +0900

    [level 1] Title: 폰켓몬, Time: 7.52 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        if(nums.length / 2 <= map.size()) return nums.length/2;
+        else return map.size();
+    }
+}
