8c5917290cef3580c623b03791461987c507f0e6
mystagogy <108065594+mystagogy@users.noreply.github.com>
2026-06-12 15:05:43 +0900

    [level 2] Title: 전화번호 목록, Time: 201.17 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            String current = phone_book[i];
+            String next = phone_book[i + 1];
+
+            if (next.startsWith(current)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
