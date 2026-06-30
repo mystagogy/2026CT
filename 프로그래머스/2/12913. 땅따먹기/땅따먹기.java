d319fb09c19cc243f4f5dc30a5b8c350194d1e50
mystagogy <108065594+mystagogy@users.noreply.github.com>
2026-06-30 16:05:16 +0900

    [level 2] Title: 땅따먹기, Time: 16.00 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int[][] land) {
+        int n = land.length;
+        
+        int[][] dp = new int[n][4];
+
+        for (int i = 0; i < 4; i++) {
+            dp[0][i] = land[0][i];
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < 4; j++) {
+                int max = 0;
+
+                for (int k = 0; k < 4; k++) {
+                    if (j == k) continue;
+
+                    max = Math.max(max, dp[i - 1][k]);
+                }
+
+                dp[i][j] = land[i][j] + max;
+            }
+        }
+        
+        int answer = 0;
+
+        for (int x : dp[n - 1]) {
+            answer = Math.max(answer, x);
+        }
+        
+        return answer;
+    }
+}
