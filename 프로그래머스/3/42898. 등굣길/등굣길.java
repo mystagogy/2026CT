263ad52458b5457e60dfd755cf5839efcc35ffeb
mystagogy <108065594+mystagogy@users.noreply.github.com>
2026-07-24 16:11:30 +0900

    [level 3] Title: 등굣길, Time: 0.72 ms, Memory: 57.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,29 @@
+class Solution {
+    static final int MOD = 1000000007;
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n][m];
+        
+        for(int[] puddle : puddles) {
+            int x = puddle[0] - 1;
+            int y = puddle[1] - 1;
+            
+            dp[y][x] = -1;
+        }
+        
+        dp[0][0] = 1;
+        
+        for(int y = 0; y < n; y++) {
+            for(int x = 0; x < m; x++) {
+                if(dp[y][x] == -1) continue;
+                
+                if(x > 0 && dp[y][x-1] != -1) dp[y][x] += dp[y][x-1];
+                
+                if(y > 0 && dp[y-1][x] != -1) dp[y][x] += dp[y-1][x];
+                
+                dp[y][x] %= MOD;
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }
+}
